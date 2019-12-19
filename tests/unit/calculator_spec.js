0d2f3ef98e3233ba5b7e9499584f75cdd442266d
Raffaele Castaldo <raffaelecastaldo88@libero.it>
2019-12-19 15:01:15 +0000

    initial commit

---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,19 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+
+  it('it can add', function(){
+    const expected = 5;
+    assert.equal( calculator.prototype.add(), expected)
+  })
+
+});
